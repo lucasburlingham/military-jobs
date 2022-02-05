ad6d569bcd71a5a3e45102e49d712a96ea350587
Lucas Burlingham <lucas.burlingham@icloud.com>
2022-02-05 15:02:11 -0500

    Use universal footer element

---
+++ footer.php
@@ -0,0 +1,38 @@
+<?php
+?>
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-4"></div>
+			<div class="col-4">
+				&copy 2022 Lucas Burlingham
+				<br>
+				Source:
+
+				<a href="https://github.com/lucasburlingham/military-jobs" style="color: black; text-decoration: none;"
+					target="_blank">
+					<i class="fab fa-github-square"></i> Github
+				</a>
+			</div>
+			<div class="col-4"></div>
+		</div>
+	</div>
+</footer>
+<div id="scripts">
+	<!-- Bootstrap JavaScript Libraries -->
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+		integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+	</script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+		integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+	</script>
+	<!-- FontAwesome 5.15.3 CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+		integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+		crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+	<!-- (Optional) Use CSS or JS implementation -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"
+		integrity="sha512-RXf+QSDCUQs5uwRKaDoXt55jygZZm2V++WUZduaU/Ui/9EGp3f/2KZVahFZBKGH0s774sd3HmrhUy+SgOFQLVQ=="
+		crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</div>
--- index.php
+++ index.php
@@ -241,35 +241,9 @@ $dbh  = new PDO($dir) or die("Cannot open the database");
 	<hr>
 	<div class="col-1"></div>
 
-	<footer>
-		<div class="container">
-			<div class="row">
-				<div class="col-4"></div>
-				<div class="col-4">
-					&copy 2022
-					<a href="https://github.com/lucasburlingham" style="color: black;"><i
-							class="fab fa-github-square"></i></a> Lucas Burlingham
-				</div>
-				<div class="col-4"></div>
-			</div>
-		</div>
-	</footer>
-	<!-- Bootstrap JavaScript Libraries -->
-	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-		integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
-	</script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
-		integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
-	</script>
-	<!-- FontAwesome 5.15.3 CSS -->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-		integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
-		crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-	<!-- (Optional) Use CSS or JS implementation -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"
-		integrity="sha512-RXf+QSDCUQs5uwRKaDoXt55jygZZm2V++WUZduaU/Ui/9EGp3f/2KZVahFZBKGH0s774sd3HmrhUy+SgOFQLVQ=="
-		crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+	<?php
+		include '/footer.php';
+	?>
 </body>
 
 </html>
